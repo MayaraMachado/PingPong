9ebf579331f851dbe753a0963289defdf3d2de8d
Mayara Machado <maaih18@gmail.com>
2017-12-26 17:02:43 -0200

    interface do jogo

--- Main.hs
+++ Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Ping Pong" (600, 600) (10, 10)
+
+background :: Color
+background = black
+
+drawing :: Picture
+drawing = pictures 
+    [ color bola $ circleSolid 15
+    , translate (-280) (-100) $ color paletaPlayer1 $ rectangleSolid 10 70
+    , translate (280) 0 $ color paletaPlayer2 $ rectangleSolid 10 70
+    , translate 0 (-300) $ color parede $ rectangleSolid 600 20
+    , translate 0 (300) $ color parede $ rectangleSolid 600 20
+    , translate (-300) (-300) $ color parede $ rectangleSolid 20 410
+    , translate (-300) (300) $ color parede $ rectangleSolid 20 410
+    , translate (300) (300) $ color parede $ rectangleSolid 20 410
+    , translate (300) (-300) $ color parede $ rectangleSolid 20 410
+    ]
+    where  
+        bola = white
+        paletaPlayer1 = green 
+        paletaPlayer2 = red
+        parede = light (light (light black))
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = display window background drawing
